38335a9412396bf468c1a642e9051a17dbb9cf3a
Saul Velasco <saulvelasco4@gmail.com>
2025-11-17 16:46:53 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
+// src/main.ts
 
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true, forbidNonWhitelisted: true }));
-  await app.listen(3000);
-  console.log('Listening on http://localhost:3000/books');
+ const app = await NestFactory.create(AppModule);
+ 
+
+ app.setGlobalPrefix('api/v1'); 
+ 
+ app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true, forbidNonWhitelisted: true }));
+ await app.listen(3000);
+
+ console.log('Listening on http://localhost:3000/api/v1/books'); 
 }
 bootstrap();
