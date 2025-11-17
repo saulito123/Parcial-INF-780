242206a696ce1fbdb605be0f428093bfe8997164
Saul Velasco <saulvelasco4@gmail.com>
2025-11-17 16:44:54 -0400

    Update books.module.ts

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,10 +1,30 @@
+// src/app.module.ts (Ejemplo de estructura)
 
 import { Module } from '@nestjs/common';
-import { BooksController } from './books.controller';
-import { BooksService } from './books.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from './book.entity';
 
 @Module({
-  controllers:[BooksController],
-  providers:[BooksService],
+  imports: [TypeOrmModule.forFeature([Book])],
+  controllers: [],
+  providers: [],
+  exports: [TypeOrmModule],
 })
 export class BooksModule {}
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'admin',
+      database: 'notes_test', 
+      entities: [Book], 
+      synchronize: true,
+    }),
+    BooksModule,
+  ],
+})
+export class AppModule {}
