e3bbd15f6b4fe873da4d5cb420ebea0fe2b656f9
Saul Velasco <saulvelasco4@gmail.com>
2025-11-17 16:43:06 -0400

    Update book.entity.ts

--- src/books/book.entity.ts
+++ src/books/book.entity.ts
@@ -1,14 +1,32 @@
+import { Entity, PrimaryGeneratedColumn, Column, Unique } from 'typeorm';
 
+@Entity('book') 
+@Unique(['titulo', 'autor']) // Aplica la restricción de unicidad para manejar 409 Conflict
 export class Book {
-  constructor(
-    public id: number,
-    public titulo: string,
-    public autor: string,
-    public isbn: string,
-    public anioPublicacion: number,
-    public categoria: string,
-    public stock: number,
-    public creadoEn?: Date,
-    public actualizadoEn?: Date,
-  ) {}
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  titulo!: string;
+
+  @Column()
+  autor!: string;
+
+  @Column()
+  isbn!: string;
+
+  @Column()
+  anioPublicacion!: number;
+
+  @Column()
+  categoria!: string;
+
+  @Column()
+  stock!: number;
+  
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  creadoEn!: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  actualizadoEn!: Date;
 }
